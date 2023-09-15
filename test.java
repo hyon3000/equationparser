4f4ca8b073fa795d0a8ec9464500975085fe5ba3
hyon3000 <fwe9849@naver.com>
2023-09-15 12:05:45 +0900

    Update test.java

--- test.java
+++ test.java
@@ -8,11 +8,17 @@ public class test {
 			System.out.println(p.compileandsolve("(a<2?b:c)=1"));
 			System.out.println(p.compileandsolve("b"));
 			System.out.println(p.compileandsolve("c"));
+			System.out.println(p.compileandsolve("reset()"));
+			System.out.println(p.compileandsolve("++++a"));
+			
 			System.out.println(p.compileandsolve("d=2+3i;f=4+5i;g=d**f"));
 			System.out.println(p.compileandsolve("max(a,4,1)"));
 			System.out.println(p.compileandsolve("true^^false"));
+			System.out.println(p.toString());
 			equation e=program.compile("a++;a+1");
-			program p2=new program(p);//copy program p
+			String str=p.toString();
+			program p2=new program();//copy program p
+			p2.fromString(str);
 			System.out.println(p2.solve(e));
 			program p3=new program(p);//copy program p
 			System.out.println(p3.solve(e));
